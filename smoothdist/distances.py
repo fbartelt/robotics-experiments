fix: derivatives working + initial planner

smoothdist/distances.py
@@ -27,9 +27,6 @@ def holder_mean(values, r=0.1, compute_gradient=False):
         inner_der = np.array(
             list(map(lambda x: -1 / r * (x ** (-1 / r - 1)), v + 1e-6))
         )
-        print("values:", values)
-        print("outer_derivative:", outer_der)
-        print("inner_derivative:", inner_der)
         gradient = outer_der * inner_der
         return res, gradient
 
@@ -92,21 +89,27 @@ def _smooth_min_list(values, r=0.1, compute_gradient=False):
         return min_value
     else:
         # ---- FORWARD + BACKWARD ----
+
+        # r = 0.1
+        # values = [-10, 10, -2, -6]
+        # print(f"Values: {values}")
+        # compute_gradient = True
         n = len(values)
         gradient = np.ones(n)
-        min_value = values[0]
-
-        for i, val in enumerate(values[1:]):
-            # compute f(prev, val) and the local grads
-            j = i + 1  # index in original array
+        min_value = values[-1]
+        local_grads = []
+        # for i, val in enumerate(reversed(values[:-1])):
+        for i in reversed(range(n - 1)):
+            val = values[i]
             min_value, local_grad = _smooth_min_two_elements(
-                min_value, val, r=r, compute_gradient=compute_gradient
+                val, min_value, r=r, compute_gradient=True
             )
-            print("Local gradient at step", j, ":", local_grad)
             left, right = local_grad
-            gradient[j - 1] *= left
-            gradient[j] *= right
-        print("Inside smooth_min_list gradient:", gradient)
+            gradient[i + 1:] *= right
+            gradient[i] *= left
+            # print(f"Step {n - i - 1}: val={val}, min_value={min_value}, local_grad={local_grad}, gradient={gradient}")
+        # print("Final min value:", min_value)
+        # print("Final gradient:", gradient)
 
         return min_value, gradient
 
@@ -323,10 +326,10 @@ def signed_dist2convex(f, p, A, b, r=0.1, h=0.5, test=None, compute_gradient=Fal
         else:
             dist = res[0]
             grad = res[1] @ raw_outer_gradients
-            print("Inside signed_dist2convex OUT gradient:", grad)
-            print("Raw outer distances:", raw_outer_distances)
-            print("Raw outer gradients:", raw_outer_gradients)
-            print("Smooth max gradient:", res[1])
+            # print("Inside signed_dist2convex OUT gradient:", grad)
+            # print("Raw outer distances:", raw_outer_distances)
+            # print("Raw outer gradients:", raw_outer_gradients)
+            # print("Smooth max gradient:", res[1])
             return dist, grad
     else:
         res_min = smooth_min(

smoothdist/planner.py
@@ -89,11 +89,16 @@ def deform_path(
             dist_, grad_ = signed_dist2convex(
                 phi, p, A, b, r=r, h=h, test=kind, compute_gradient=True
             )
+            print(f"Pre dist: {dist_}")
             # print(f"Point {j}, Obstacle {i}, dist: {dist_}, grad: {grad_.ravel()}")
+            grad_sat = (1 / (1 + np.exp(alpha * dist_)))
             dist_ = (
                 (-1 / alpha) * np.log(0.5 * (1 + np.exp(-alpha * dist_)))
             )  # Smooth saturation
-            grad_ = grad_ * (1 / (1 + np.exp(alpha * dist_)))
+            print(f"Post dist: {dist_}")
+            print(f"Pre grad: {grad_.ravel()}")
+            grad_ = grad_ * grad_sat
+            print(f"Post grad: {grad_.ravel()}")
             # print(f"Saturated dist: {dist_}, grad: {grad_.ravel()}")
             dists_[i] = np.round(dist_, 6)
             if np.round(dist_, 4) >= 0:
@@ -115,10 +120,11 @@ def deform_path(
         # coeff = np.abs(dists[j])  # np.sign(dists[j]) * np.sqrt(np.abs(dists[j]))
         coeff = 1.0
         if dists[k] > 0:
-            coeff = np.sqrt(np.abs(dists[k]))
+            coeff = 1.0
+            # coeff = np.sqrt(np.abs(dists[k]))
         else:
             coeff = np.sqrt(np.abs(dists[k]))
-        path[:, k] += grads[k] * 1
+        path[:, k] += grads[k] * coeff
 
     if min_path:
         for j, point in enumerate(path.T[1:-1]):
@@ -139,8 +145,8 @@ bounding_box = (-20.0, -20, 20, 20)
 radius_limits = (2, 6)
 q0 = np.array([-3.0, -12]).reshape(-1, 1)
 q0 = np.array([-8.5, -16]).reshape(-1, 1)
-# qd = np.array([15, 15]).reshape(-1, 1)
-qd = np.array([11.0, 15]).reshape(-1, 1)
+qd = np.array([18, 15]).reshape(-1, 1)
+# qd = np.array([11.0, 15]).reshape(-1, 1)
 n_points = 100
 h = 0.01
 r = 0.1
@@ -174,15 +180,17 @@ path = init_path.copy()
 dists = [-100]
 iter_ = 0
 path_hist = [init_path.copy()]
-max_iters = 10
+max_iters = 300
 kind = "in"
-# kind = None
+kind = "out"
+kind = None
 
 fig = go.Figure()
 for obstacle in obstacles:
     add_polygon(fig, obstacle.A, obstacle.b, add_reference=False)
 
-while np.any(np.array(dists) < 2.0):
+obstacles = [obstacles[0]]
+while np.any(np.array(dists) < 0.0):
     if iter_ >= max_iters:
         print(f"reached max iterations: {max_iters}")
         break
@@ -205,24 +213,25 @@ fig.show()
 # [jump]
 
 # %%
-# print(dists)
-fig = create_planning_plot(
-    constraints,
-    pc_list,
-    R_list,
-    q0,
-    qd,
-    path,
-    init_path,
-    bbox=bounding_box,
-    n_points=n_points,
-    n_points_contour=200,
-    n_countours=40,
-    eps=eps,
-    r=r,
-    h=h,
-    eta=eta,
-    bulge=bulge,
-    plot_cicles=False,
-)
-fig.show()
+x = np.linspace(-1, 1, 200)
+alpha = np.log(2) / 0.01
+
+go.Figure(
+    data=go.Scatter(
+        x=x,
+        y=(-1 / alpha) * np.log(0.5 * (1 + np.exp(-alpha * x))),
+        mode="lines",
+        line=dict(color="blue", width=2),
+        name="Smooth Saturation",
+)).show()
+
+
+
+go.Figure(
+    data=go.Scatter(
+        x=x,
+        y=1 / (1 + np.exp(alpha * x)),
+        mode="lines",
+        line=dict(color="blue", width=2),
+        name="Saturation Gradient",
+)).show()

smoothdist/test_derivatives.py
@@ -51,7 +51,8 @@ print("############# holder mean end")
 
 """ Smoooth min test """
 for i in range(10):
-    vals = rng.uniform(-10., 10, size=(4,))
+    vals = rng.uniform(-100., 100, size=(10,))
+    # vals = np.array([-29.0, -30.0, -50.0, -10.0])
     # vals = np.array([2, 3.0])
     # vals = rng.uniform(0, 10, size=(2,))
     # vals = rng.uniform(-10, 10, size=(2,))
@@ -66,13 +67,16 @@ for i in range(10):
         vals_plus[j] += eps
         vals_minus[j] -= eps
         value_plus = smooth_min(
-            vals_plus[0], vals_plus[1], r=0.1, compute_gradient=False
+                vals_plus, r=0.1, compute_gradient=False
         )
         value_minus = smooth_min(
-            vals_minus[0], vals_minus[1], r=0.1, compute_gradient=False
+                vals_minus, r=0.1, compute_gradient=False
         )
         num_derivative[j] = (value_plus - value_minus) / (2 * eps)
+    derivative = np.round(derivative, 6)
+    num_derivative = np.round(num_derivative, 6)
     error = np.linalg.norm(derivative - num_derivative)
+    # print(f"Iteration {i}, values: {vals}, min value: {value}, error: {error}\ngradient: {derivative}, num_gradient: {num_derivative}")
     if error > 1e-3:
         print(f"vals:{vals}")
         print(f"value: {value}")
@@ -85,7 +89,7 @@ print("############# smooth min end")
 
 """ Smoooth max test """
 for i in range(10):
-    vals = rng.uniform(-10., 10, size=(4,))
+    vals = rng.uniform(-100., 100, size=(4,))
     # vals = np.array([2, 3.0])
     # vals = rng.uniform(0, 10, size=(2,))
     # vals = rng.uniform(-10, 10, size=(2,))
@@ -100,10 +104,10 @@ for i in range(10):
         vals_plus[j] += eps
         vals_minus[j] -= eps
         value_plus = smooth_max(
-            vals_plus[0], vals_plus[1], r=0.1, compute_gradient=False
+                vals_plus, r=0.1, compute_gradient=False
         )
         value_minus = smooth_max(
-            vals_minus[0], vals_minus[1], r=0.1, compute_gradient=False
+                vals_minus, r=0.1, compute_gradient=False
         )
         num_derivative[j] = (value_plus - value_minus) / (2 * eps)
     error = np.linalg.norm(derivative - num_derivative)
@@ -140,18 +144,27 @@ polygon3 = Polytope.random(
     num_vertices=6, radius_lim=(1e-1, 1.0), bbox=bounding_box, seed=seed + 2
 )
 
+A_test = np.array([
+    [1.0, 0.0],
+    [0.0, 1.0],
+    [-1.0, 0.0],
+    [0.0, -1.0],
+    ])
+b_test = np.array([2.0, 2.0, 2.0, 2.0])
+polygon_test = Polytope(A_test, b_test)
 polygons = [
-    polygon,
+    polygon_test,
 ]
 
 vertices = polygon.vertices
 # bounding_box = (vertices.min(), vertices[:, 1].min(),
 #                 vertices.max(), vertices[:, 1].max())
-bounding_box = (-100, -100, -20, -20)
+bounding_box = (-20, -20, 20, 20)
 xs = np.linspace(bounding_box[0], bounding_box[2], n_points)
 ys = np.linspace(bounding_box[1], bounding_box[3], n_points)
 
 test = "out"
+# test = "in"
 test = None
 eps = 1e-5
 