f2006ce1c4f60be53b19b2b014de5a7d24a2c684
fbartelt <fbarteltp@gmail.com>
2026-02-07 11:38:12 -0300

    fix: use vector task function (no limit cycles)

--- smoothdist/control_case.py
+++ smoothdist/control_case.py
@@ -33,6 +33,7 @@ from path_planning import (
 )
 from uaibot.utils import Utils
 
+
 def add_point(fig, p, color="black", name="point"):
     fig.add_trace(
         go.Scatter(
@@ -44,6 +45,31 @@ def add_point(fig, p, color="black", name="point"):
         )
     )
 
+
+def add_vector_at_point(fig, p, v, color="black", name="vector"):
+    fig.add_trace(
+        go.Scatter(
+            x=[p[0], p[0] + v[0]],
+            y=[p[1], p[1] + v[1]],
+            mode="lines",
+            line=dict(color=color, width=2),
+            marker=dict(size=4, color=color),
+            name=name,
+            showlegend=False,
+        )
+    )
+    # Add triangle head to indicate direction
+    head_size = 0.1
+    fig.add_trace(
+        go.Scatter(
+            x=[p[0] + v[0]],
+            y=[p[1] + v[1]],
+            mode="markers",
+            marker=dict(size=8, color=color, symbol="triangle-up"),
+        )
+    )
+
+
 def dist2set(q, obstacles, r=0.1, h=1e-2, method="ours"):
     dists = np.zeros(len(obstacles))
     grads = np.zeros((q.shape[0], len(obstacles)))  # n x m
@@ -54,24 +80,92 @@ def dist2set(q, obstacles, r=0.1, h=1e-2, method="ours"):
         if method == "esdf":
             dist_ij, grad, closest_pt = ESDF_CGAL(p_i, obs.A, obs.b.reshape(-1, 1))
         else:
-            try: 
+            try:
                 dist_ij, grad = signedDist2Convex(
-                    p_i, obs.A, obs.b.reshape(-1, 1), r=r, eps=h, test="both"
+                    p_i,
+                    obs.A,
+                    obs.b.reshape(-1, 1),
+                    r=r,
+                    eps=h,
                 )
             except:
                 print(f"Failed signedDist at point {p_i.flatten()} for obstacle {j}")
-        dists[j] = dist_ij
-        grads[:, j] = grad.reshape(-1)
+                raise ValueError("Signed distance computation failed.")
+        factor_ = 1.0
+        dists[j] = dist_ij * factor_
+        grads[:, j] = grad.reshape(-1) * factor_
 
     if method == "esdf":
         id_min = np.argmin(dists)
         min_dist = min(dists)
-        min_grad = -grads[:, id_min]
+        min_grad = grads[:, id_min]
     else:
         min_dist, smooth_min_grad = smoothMinListWithGradient(dists, r=r)
-        min_grad = -(grads @ smooth_min_grad.reshape(-1, 1))  # n x 1
+        min_grad = grads @ smooth_min_grad.reshape(-1, 1)  # n x 1
 
     return min_dist, min_grad
+
+
+def cbf_2nd_test(q, q_dot, qd, circ_rad, circ_center, eps, k, eta, gamma=1e-3):
+    r"""
+    min ||dr/dq u + 2k dr/dq qdot + k^2r + qdot^T d^2r/dq^2 qdot||^2 + eps ||u||^2
+    s.t.
+        \ddot{B} \ge -2\eta\dot{B} - \eta^2 B
+    -->
+        min ||dr/dq u + 2k dr/dq qdot + k^2r + qdot^T d^2r/dq^2 qdot||^2 + eps ||u||^2
+    """
+    qd = qd.reshape(-1, 1)
+    q_dot = q_dot.reshape(-1, 1)
+    p_i = q.reshape(-1, 1)
+    r_task = p_i - qd  # 1 x N vector
+    drdq = np.eye(q.shape[0])
+    # d2rdq2 is a null tensor
+
+    # B(q) will be the distance to the obstacles
+    dist_vec = p_i - circ_center.reshape(-1, 1)
+    dist2center = np.linalg.norm(dist_vec)
+    B = dist2center - circ_rad
+    dBdq = dist_vec / (dist2center + 1e-6)
+    dot_B = (dBdq.T @ q_dot).item()
+    p_next = p_i + gamma * q_dot
+    p_prev = p_i - gamma * q_dot
+    dist_vec_prev = p_prev - circ_center.reshape(-1, 1)
+    dist_vec_next = p_next - circ_center.reshape(-1, 1)
+    dist2center_prev = np.linalg.norm(dist_vec_prev)
+    dist2center_next = np.linalg.norm(dist_vec_next)
+    dBdq_prev = dist_vec_prev / (dist2center_prev + 1e-6)
+    dBdq_next = dist_vec_next / (dist2center_next + 1e-6)
+    quad_term = (dBdq_next - dBdq_prev).T / (2 * gamma) @ q_dot
+
+    A = dBdq.T
+    b = -2 * eta * dot_B - eta**2 * B - quad_term
+
+    # Uaibot uses 1/2 u^T H u + f^T u s.t. Au >= b
+    # Functional
+    H = 2.0 * (eps * np.eye(q.shape[0])) + (drdq @ drdq.T)
+    f_transpose = (
+        2 * k * drdq.T @ q_dot
+        + k**2 * r_task  # + q_dot.T @ d2rdq2 @ q_dot # This is zero
+    ).T @ drdq.T
+    f_transpose = f_transpose.reshape(1, -1)
+
+    u = Utils.solve_qp(H, f_transpose, A, b)
+    u = np.array(u).reshape(-1, 1)
+    obj_test = u.T @ H @ u + f_transpose @ u
+    constraint_test = A @ u - b
+    flag = False
+
+    if constraint_test.flatten()[0] <= 0:
+        flag = True
+        # print(
+        #     f"Constraint violation: Au = {(A @ u).flatten()[0]}, b = {b.flatten()[0]}"
+        # )
+        # print(
+        #     f"At point {p_i.flatten()}. Current dB/dq: {dBdq.flatten()}. dot_B: {dot_B.flatten()[0]}, B: {B}, quad_term: {quad_term.flatten()[0]}. u: {u.flatten()}"
+        # )
+    # print(f"Objective: {obj_test.flatten()[0]}")
+    # print(f"Constraint (should be >=0): {constraint_test.flatten()[0]}")
+    return u, dBdq, flag
 
 
 def cbf_2nd(q, q_dot, qd, obstacles, eps, k, eta, r, h, method="ours", gamma=1e-3):
@@ -82,44 +176,58 @@ def cbf_2nd(q, q_dot, qd, obstacles, eps, k, eta, r, h, method="ours", gamma=1e-
     -->
         min ||dr/dq u + 2k dr/dq qdot + k^2r + qdot^T d^2r/dq^2 qdot||^2 + eps ||u||^2
     """
-    r_task = 0.5 * np.linalg.norm(q - qd) ** 2
+    qd = qd.reshape(-1, 1)
+    q_dot = q_dot.reshape(-1, 1)
     p_i = q.reshape(-1, 1)
+    r_task = p_i - qd  # 1 x N vector
+    drdq = np.eye(q.shape[0])
+    # d2rdq2 is a null tensor
+
     # B(q) will be the distance to the obstacles
     B, dBdq = dist2set(p_i, obstacles, r=r, h=h, method=method)
-    dot_B = dBdq.T @ q_dot
-    drdq = q - qd
-    d2rdq2 = np.eye(q.shape[0])
-    # Uaibot uses 1/2 u^T H u + f^T u
-    # Functional
-    H = 2.0 * (eps * np.eye(q.shape[0]) + drdq @ drdq.T)
-    f_transpose = (
-        2 * (2 * k * drdq.T @ q_dot + k**2 * r_task + q_dot.T @ d2rdq2 @ q_dot).T @ drdq.T
-    )
-    # Constraint
     # dBdq u >= -2 eta dot_B - eta^2 B - qdot^T d2Bdq2 qdot
-    d2Bdq2 = np.zeros((q.shape[0], q.shape[0]))
-    for i in range(q.shape[0]):
-        e_i = np.zeros((q.shape[0], 1))
-        e_i[i, 0] = 1.0
-        _, dBdq_prev = dist2set(p_i - gamma * e_i, obstacles, r=r, h=h, method=method)
-        _, dBdq_next = dist2set(p_i + gamma * e_i, obstacles, r=r, h=h, method=method)
-        d2Bdq2[:, i] = (dBdq_next - dBdq_prev).reshape(-1) / (2 * gamma)
+    # d2Bdq2 = np.zeros((q.shape[0], q.shape[0]))
+    # for i in range(q.shape[0]):
+    #     e_i = np.zeros((q.shape[0], 1))
+    #     e_i[i, 0] = 1.0
+    #     _, dBdq_prev = dist2set(p_i - gamma * e_i, obstacles, r=r, h=h, method=method)
+    #     _, dBdq_next = dist2set(p_i + gamma * e_i, obstacles, r=r, h=h, method=method)
+    #     d2Bdq2[:, i] = (dBdq_next - dBdq_prev).reshape(-1) / (2 * gamma)
+    #
+    # quad_term = q_dot.T @ d2Bdq2 @ q_dot
+    dot_B = dBdq.T @ q_dot
+    _, dBdq_prev = dist2set(p_i - gamma * q_dot, obstacles, r=r, h=h, method=method)
+    _, dBdq_next = dist2set(p_i + gamma * q_dot, obstacles, r=r, h=h, method=method)
+    quad_term = (dBdq_next - dBdq_prev).T / (2 * gamma) @ q_dot
 
     A = dBdq.T
-    b = -2 * eta * dot_B - eta**2 * B - q_dot.T @ d2Bdq2 @ q_dot
-    # A_eq = np.zeros((1, q.shape[0]))
-    # b_eq = np.zeros((1, 1))
-    # print(f"A.shape={A.shape}, b.shape={b.shape}, A_eq.shape={A_eq.shape}, b_eq.shape={b_eq.shape}")
-    # print(f"H.shape={H.shape}, f_transpose.shape={f_transpose.shape}")
-    # np.vstack([A, A_eq])
-    # print(A.shape, b.shape, q.shape)
+    b = -2 * eta * dot_B - eta**2 * B - quad_term
+
+    ## Uaibot uses 1/2 u^T H u + f^T u s.t. Au >= b
+    # Functional
+    H = 2.0 * (eps * np.eye(q.shape[0])) + (drdq @ drdq.T)
+    f_transpose = (
+        2 * k * drdq.T @ q_dot
+        + k**2 * r_task  # + q_dot.T @ d2rdq2 @ q_dot # This is zero
+    ).T @ drdq.T
+    f_transpose = f_transpose.reshape(1, -1)
+
     u = Utils.solve_qp(H, f_transpose, A, b)
     u = np.array(u).reshape(-1, 1)
     obj_test = u.T @ H @ u + f_transpose @ u
     constraint_test = A @ u - b
+    flag = False
+    if constraint_test.flatten()[0] <= 0:
+        flag = True
+        # print(
+        #     f"Constraint violation: Au = {(A @ u).flatten()[0]}, b = {b.flatten()[0]}"
+        # )
+        # print(
+        #     f"At point {p_i.flatten()}. Current dB/dq: {dBdq.flatten()}. dot_B: {dot_B.flatten()[0]}, B: {B}, quad_term: {quad_term.flatten()[0]}. u: {u.flatten()}"
+        # )
     # print(f"Objective: {obj_test.flatten()[0]}")
     # print(f"Constraint (should be >=0): {constraint_test.flatten()[0]}")
-    return u
+    return u, dBdq, flag
 
 
 A1 = np.array(
@@ -133,8 +241,15 @@ A1 = np.array(
 
 # Room-like environment
 bounding_box = (-12.0, -10.0, 7.0, 10.0)
-q0 = np.array([-8., -4.]).reshape(-1, 1)  # 1111
-qd = np.array([1.6, 8.0]).reshape(-1, 1)  # 1111
+q0 = np.array([-8.0, -4.0]).reshape(-1, 1)  # 1111
+qd = np.array([1.6, 5.0]).reshape(-1, 1)  # 1111
+qd = np.array([1.6, 8.0]).reshape(-1, 1)  # LImit CYcle
+# qd = np.array([-10.6, 8.0]).reshape(-1, 1)  # Works
+# qd = np.array([-3.5, 8.0]).reshape(-1, 1)  # LImit CYcle
+# qd = np.array([-4.1, 8.0]).reshape(-1, 1)  # Works
+# qd = np.array([6.0, 2.0]).reshape(-1, 1)
+# qd = np.array([-3.0, 4.0]).reshape(-1, 1)
+
 left_wall = np.array([-5, 7.0, 5.0, 5.0])
 o1 = Polytope(A1, left_wall)
 top_wall = np.array([3.0, 7.0, 7.0, -5.0])
@@ -163,34 +278,59 @@ for i in range(3):
         o_desk = Polytope(A1, desk_i)
         obstacles.append(o_desk)
 
-# obstacles = [o6]
+far_obj = desk_base + A1 @ np.array([20.0, -20.0])
+o7 = Polytope(A1, far_obj)
+
+obstacles = [o6]
 # Second order kinematic control for a point mass
 reached = False
 q = q0.copy()
 q_dot = np.zeros((2, 1))
 dt = 1e-3
-hist = [q.copy().T]
 # 10 seconds max based on dt
-max_iters = int(15.0 / dt) / 2
-# max_iters = 1000
+max_iters = int(10.0 / dt) * 2.0
+# max_iters = 1700
 # max_iters = 1
 iter = 0
-r = 0.1
+r = 1e-1
 h = 1e-2
-eta = 0.5
+eta = 0.5   # * 25.0
 eps = 1e-3
-k = 2.0
+k = 2.0 * 1
+gamma = dt
 method = "ours"
 # method = "esdf"
+
+hist = [q.copy().T]
+hist_grad = [q.copy().T * 0]
+flags = [False]
+flag = False
+
+circ_rad = 2
+circ_center = np.array([-3.0, 2.0])
 
 while not reached:
     # Compute u using a second-order CBF
     if np.linalg.norm(q - qd) < 1e-2:
         reached = True
         print("Reached the goal!")
+    if flag:
+        pass
+        # break
 
     # try:
-    u = cbf_2nd(
+    # u, grad, flag = cbf_2nd_test(
+    #     q,
+    #     q_dot,
+    #     qd,
+    #     circ_rad=circ_rad,
+    #     circ_center=circ_center,
+    #     eps=eps,
+    #     k=k,
+    #     eta=eta,
+    #     gamma=gamma,
+    # )
+    u, grad, flag = cbf_2nd(
         q,
         q_dot,
         qd,
@@ -201,7 +341,7 @@ while not reached:
         r=r,
         h=h,
         method=method,
-        gamma=dt,
+        gamma=gamma,
     )
     if iter % 1000 == 0:
         print(u)
@@ -218,19 +358,18 @@ while not reached:
     #
     #     break
     q += q_dot * dt
-    q_dot += u * dt
+    q_dot = q_dot + u * dt
     # q_dot = -k * (q - qd)
     # q_dot += u * dt
     hist.append(q.copy().T)
+    hist_grad.append(grad.copy().T)
+    flags.append(flag)
     iter += 1
     if iter >= max_iters:
         print("Max iterations reached, stopping simulation.")
         break
 
-print(q)
-print("a")
-print(len(hist))
-np.array(hist).reshape(-1, 2).shape
+print(f"Number of iterations: {iter}, number of constraint violations: {sum(flags)}")
 # %%
 fig, cmap_data = create_level_sets(
     obstacles,
@@ -252,13 +391,30 @@ add_point(fig, q0.flatten(), color="green", name="Start")
 add_point(fig, qd.flatten(), color="red", name="Goal")
 # Convert hist of points to a 2 x N path
 full_path = np.array(hist).reshape(-1, 2).T
-fig.add_trace(
-    go.Scatter(
-        x=full_path[0, :],
-        y=full_path[1, :],
-        mode="lines",
-        line=dict(color="black", width=2),
-        name="Trajectory",
+
+
+def f(fig):
+    fig.add_trace(
+        go.Scatter(
+            x=full_path[0, :],
+            y=full_path[1, :],
+            mode="lines",
+            line=dict(color="black", width=2),
+            name="Trajectory",
         )
     )
+
+
+f(fig)
+
+# max_grads = 100
+# plot_idxs = np.linspace(0, len(hist) - 1, max_grads).astype(int)
+# for i, (p, grad_) in enumerate(zip(hist, hist_grad)):
+#     if i not in plot_idxs:
+#         continue
+#     norm_ = np.linalg.norm(grad_)
+#     norm_ = max(norm_, 1e-6)
+#     grad = grad_ / norm_ * min(norm_, 0.5)
+#     add_vector_at_point(fig, p.flatten(), grad.flatten(), color="blue", name="Grad")
+
 fig.show()
