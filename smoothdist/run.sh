ff6b00e77cc9aecc3064a13d220bc743f225cea3
fbartelt <fbarteltp@gmail.com>
2026-05-11 16:54:28 -0300

    chore: move old code into deprecated/ and useful to geometry/
    Files in deprecated/ should either be deleted in the future or
    incorporated in other functions

--- smoothdist/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Delete nohup.out if it exists
-source ../../debugenv/bin/activate
-rm -f nohup.out
-# Run the Python script in the background with nohup
-nohup python -u planner_stats.py &
